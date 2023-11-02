725200f0ebe022c301cd83dd18818e36bf2f4683
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2023-11-02 00:01:04 -0500

    Add Docker Compose, basic logging for startup and shutdown

--- src/main/java/tv/galaxe/genesis/Genesis.java
+++ src/main/java/tv/galaxe/genesis/Genesis.java
@@ -2,16 +2,19 @@ package tv.galaxe.genesis;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class Genesis extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getLogger().log(Level.INFO, "Genesis has been enabled!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getServer().getLogger().log(Level.INFO, "Genesis has been disabled!");
     }
 }
