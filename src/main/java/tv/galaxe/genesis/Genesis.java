a1ed229ec7579b7bd334c193d34e90accf3fe686
Garrett Summerfield <garrettsummerfi3ld@gmail.com>
2023-11-01 23:54:04 -0500

    Initial commit

---
+++ src/main/java/tv/galaxe/genesis/Genesis.java
@@ -0,0 +1,17 @@
+package tv.galaxe.genesis;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Genesis extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
