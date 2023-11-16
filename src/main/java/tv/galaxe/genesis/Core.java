a131deb19311f5223df71d49371af770d759b9d2
Josef Baltz <josef.baltz@proton.me>
2023-11-15 23:34:19 -0500

    Phantom functionality

--- src/main/java/tv/galaxe/genesis/Core.java
+++ src/main/java/tv/galaxe/genesis/Core.java
@@ -3,8 +3,7 @@ package tv.galaxe.genesis;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import tv.galaxe.genesis.event.enforcer.Enderman;
-import tv.galaxe.genesis.event.enforcer.Skeleton;
+import tv.galaxe.genesis.event.enforcer.Phantom;
 
 public final class Core extends JavaPlugin implements Listener {
 	public static Plugin plugin;
@@ -19,8 +18,9 @@ public final class Core extends JavaPlugin implements Listener {
 		// getCommand("genesis").setExecutor(new GenesisGUI());
 
 		// Genus Enforcers
-		getServer().getPluginManager().registerEvents(new Skeleton(), this);
-		getServer().getPluginManager().registerEvents(new Enderman(), this);
+		// getServer().getPluginManager().registerEvents(new Skeleton(), this);
+		// getServer().getPluginManager().registerEvents(new Enderman(), this);
+		getServer().getPluginManager().registerEvents(new Phantom(), this);
 	}
 
 	@Override
