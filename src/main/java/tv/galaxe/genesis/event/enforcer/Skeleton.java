28f5d58509a6bb0c03ceefe87b54ea73d4102a72
Josef Baltz <josef.baltz@proton.me>
2023-11-19 22:36:27 -0500

    remove unused import

--- src/main/java/tv/galaxe/genesis/event/enforcer/Skeleton.java
+++ src/main/java/tv/galaxe/genesis/event/enforcer/Skeleton.java
@@ -9,7 +9,6 @@ import org.bukkit.entity.AbstractSkeleton;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Trident;
-import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
