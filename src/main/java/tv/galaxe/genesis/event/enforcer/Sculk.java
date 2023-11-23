33ddeef42cfe5389ac8b877e9fa0bc09ea8a0adf
Josef Baltz <josef.baltz@proton.me>
2023-11-23 02:18:28 -0600

    Finalize sculk

--- src/main/java/tv/galaxe/genesis/event/enforcer/Sculk.java
+++ src/main/java/tv/galaxe/genesis/event/enforcer/Sculk.java
@@ -1,13 +1,18 @@
 package tv.galaxe.genesis.event.enforcer;
 
+import java.util.EnumSet;
 import java.util.HashMap;
 import org.bukkit.GameMode;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Warden;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
@@ -19,6 +24,11 @@ import tv.galaxe.genesis.runnable.SculkRunnable;
 
 public class Sculk implements Listener {
 	private static HashMap<Player, BukkitTask> taskMap = new HashMap<Player, BukkitTask>();
+
+	EnumSet<Material> nonVegetarian = EnumSet.of(Material.COOKED_BEEF, Material.COOKED_CHICKEN, Material.COOKED_COD,
+			Material.COOKED_MUTTON, Material.COOKED_PORKCHOP, Material.COOKED_RABBIT, Material.COOKED_SALMON,
+			Material.BEEF, Material.CHICKEN, Material.COD, Material.MUTTON, Material.PORKCHOP, Material.RABBIT,
+			Material.SALMON, Material.ROTTEN_FLESH, Material.TROPICAL_FISH, Material.PUFFERFISH, Material.RABBIT_STEW);
 
 	@EventHandler
 	public void onConnect(PlayerJoinEvent event) {
@@ -42,17 +52,44 @@ public class Sculk implements Listener {
 
 	@EventHandler
 	public void actionKey(PlayerSwapHandItemsEvent event) {
-		if (event.getPlayer().hasPermission("genesis.classes.enderman")
+		if (event.getPlayer().hasPermission("genesis.classes.sculk")
 				&& event.getPlayer().getGameMode().equals(GameMode.SURVIVAL)) {
 			event.setCancelled(true);
-			if (event.getPlayer().getLevel() > 0
-					&& event.getPlayer().getHealth() < Core.plugin.getConfig().getDouble("classes.sculk.max-health")) {
-				event.getPlayer().giveExpLevels(-Core.plugin.getConfig().getInt("classes.sculk.ability.level-cost"));
-				event.getPlayer().setHealth(event.getPlayer().getHealth()
-						+ Core.plugin.getConfig().getDouble("classes.sculk.ability.health-reward"));
-				event.getPlayer().getWorld().playSound(((Entity) event.getPlayer()), Sound.BLOCK_SCULK_CHARGE, 1.0F,
-						1.0F);
+			double healthDiff = (Core.plugin.getConfig().getDouble("classes.sculk.max-health")
+					- event.getPlayer().getHealth()) / 2.0;
+			if (event.getPlayer().getLevel() > 0) {
+				if (healthDiff >= 1.0) {
+					event.getPlayer()
+							.giveExpLevels(-Core.plugin.getConfig().getInt("classes.sculk.ability.level-cost"));
+					event.getPlayer().setHealth(event.getPlayer().getHealth()
+							+ Core.plugin.getConfig().getDouble("classes.sculk.ability.health-reward"));
+					event.getPlayer().getWorld().playSound(((Entity) event.getPlayer()), Sound.BLOCK_ANVIL_LAND, 0.5F,
+							1.5F);
+				} else if (healthDiff != 0.0) {
+					event.getPlayer()
+							.giveExpLevels(-Core.plugin.getConfig().getInt("classes.sculk.ability.level-cost"));
+					event.getPlayer().setHealth(Core.plugin.getConfig().getDouble("classes.sculk.max-health"));
+					event.getPlayer().getWorld().playSound(((Entity) event.getPlayer()), Sound.BLOCK_ANVIL_LAND, 0.5F,
+							1.5F);
+				}
 			}
+		}
+	}
+
+	@EventHandler
+	public void onMobTarget(EntityTargetLivingEntityEvent event) {
+		if (event.getTarget() != null && event.getTarget().hasPermission("genesis.classes.sculk")
+				&& event.getEntity() instanceof Warden) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler
+	public void onConsume(PlayerItemConsumeEvent event) {
+		if (event.getPlayer().hasPermission("genesis.classes.sculk")
+				&& nonVegetarian.contains(event.getItem().getType())) {
+			event.setCancelled(true);
+			event.getPlayer().playSound(event.getPlayer(), Sound.ENTITY_VILLAGER_NO, 1.0F, 1.0F);
 		}
 	}
 }
--- src/main/java/tv/galaxe/genesis/runnable/SculkRunnable.java
+++ src/main/java/tv/galaxe/genesis/runnable/SculkRunnable.java
@@ -1,6 +1,10 @@
 package tv.galaxe.genesis.runnable;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import tv.galaxe.genesis.Core;
 
 public class SculkRunnable implements Runnable {
 	private Player player;
@@ -11,6 +15,13 @@ public class SculkRunnable implements Runnable {
 
 	@Override
 	public void run() {
-		// TODO: Sculk Looping Functions
+		if (player.getGameMode().equals(GameMode.SURVIVAL) && player.getWorld().isDayTime()
+				&& !(player.getLocation().getBlock().getLightFromSky() < 15)
+				&& player.getEquipment().getHelmet() == null) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 60,
+					Core.plugin.getConfig().getInt("classes.sculk.mining-fatigue-level")));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 60,
+					Core.plugin.getConfig().getInt("classes.sculk.slowness-level")));
+		}
 	}
 }
